7f1e5a0e1f2618bed6d345dbe60525b2b33602ce
Dimid Duchovny <dimidd@gmail.com>
2016-12-14 15:15:37 +0200

    plugin: update toponyms file
    Signed-off-by: Dimid Duchovny <dimidd@gmail.com>

--- src/main/java/org/pelagios/recogito/sdk/examples/ner/ExampleKimaNERPlugin.java
+++ src/main/java/org/pelagios/recogito/sdk/examples/ner/ExampleKimaNERPlugin.java
@@ -15,16 +15,15 @@ import org.pelagios.recogito.sdk.ner.NERPlugin;
 import org.pelagios.recogito.sdk.ner.Entity;
 
 
-public class ExampleKimaNERPlugin implements NERPlugin {
+public class ExampleKimaNERPlugin implements NERPlugin, HebMatcher {
 
-  private Set<String> _aliases;
-  private char [] _prefs = {'ב','מ','ל'};
+    private Set<String> _aliases;
+    private char [] _prefs = {'ב', 'מ', 'ל', 'ש', 'ו'};
 
-  public ExampleKimaNERPlugin() {
+    public ExampleKimaNERPlugin() {
     try {
         Arrays.sort(_prefs);
-        //BufferedReader in = new BufferedReader(new FileReader("nli.txt"));
-        InputStream input = getClass().getResourceAsStream("/nli.txt");
+        InputStream input = getClass().getResourceAsStream("/kima.txt");
         BufferedReader in = new BufferedReader(new InputStreamReader(input));
         String str;
 
@@ -38,43 +37,43 @@ public class ExampleKimaNERPlugin implements NERPlugin {
         e.printStackTrace();
         _aliases = new HashSet();
     }
-  }
+    }
 
-  @Override
-  public String getName() {
+    @Override
+    public String getName() {
     return "Example Kima NER Plugin";
-  }
+    }
 
-  @Override
-  public String getDescription() {
+    @Override
+    public String getDescription() {
     return "An attempt to use Kima with the Recogito NER plugin interface.";
-  }
+    }
 
-  @Override
-  public String getOrganization() {
+    @Override
+    public String getOrganization() {
     return "Pelagios Commons";
-  }
+    }
 
-  @Override
-  public String getVersion() {
+    @Override
+    public String getVersion() {
     return "1.0";
-  }
+    }
 
-  @Override
-  public List<String> getSupportedLanguages() {
+    @Override
+    public List<String> getSupportedLanguages() {
     List<String> list = new ArrayList<String>();
     list.add("he");
 
     return list;
-  }
+    }
 
-  /**
-   * For testing and demonstration purposes, the dummy NER plugin
-   * reports a 'LOCATION' entity for every uppercase word. Don't consider
-   * this a sensible
-   */
-  @Override
-  public List<Entity> parse(String text) {
+    /**
+    * For testing and demonstration purposes, the dummy NER plugin
+    * reports a 'LOCATION' entity for every uppercase word. Don't consider
+    * this a sensible
+    */
+    @Override
+    public List<Entity> parse(String text) {
     String[] words = text.split("\\s+");
     List<Entity> phrases = new ArrayList<Entity>();
 
@@ -88,7 +87,7 @@ public class ExampleKimaNERPlugin implements NERPlugin {
       String nextWord = words[runningIdx];
       runningOffset = text.indexOf(nextWord, runningOffset);
 
-      if (hasWord(nextWord)) {
+      if (hasWord(nextWord, _aliases)) {
         phrases.add(new Entity(
         nextWord,
         EntityType.LOCATION,
@@ -101,23 +100,6 @@ public class ExampleKimaNERPlugin implements NERPlugin {
     }
 
     return phrases;
-  }
-
-   public boolean hasWord(String word) {
-    // remove punctuation
-    word = word.replaceAll("\\p{P}", "");
-    if (_aliases.contains(word)) {
-        return true;
     }
-    else {
-        if (word != null && !word.isEmpty()) {
-        	char letter = word.charAt(0);
-            return  Arrays.binarySearch(_prefs, letter) > -1 && _aliases.contains(word.substring(1));
-        }
-        else {
-            return false;
-        }
-    }
-  }
 
 }
