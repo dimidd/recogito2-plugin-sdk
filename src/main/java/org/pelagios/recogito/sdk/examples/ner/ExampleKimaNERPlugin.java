434aec03cffebca777b821608adb7a53a516b342
Dimid Duchovny <dimidd@gmail.com>
2018-03-10 22:57:20 +0200

    WIP
    Signed-off-by: Dimid Duchovny <dimidd@gmail.com>

--- src/main/java/org/pelagios/recogito/sdk/examples/ner/ExampleKimaNERPlugin.java
+++ src/main/java/org/pelagios/recogito/sdk/examples/ner/ExampleKimaNERPlugin.java
@@ -16,90 +16,102 @@ import org.pelagios.recogito.sdk.ner.Entity;
 
 
 public class ExampleKimaNERPlugin implements NERPlugin, HebMatcher {
+	public static final int MAX_WORDS = 7;
+	private Set<String> _aliases;
+	private char [] _prefs = {'ב', 'מ', 'ל', 'ש', 'ו'};
 
-    private Set<String> _aliases;
-    private char [] _prefs = {'ב', 'מ', 'ל', 'ש', 'ו'};
+	public ExampleKimaNERPlugin() {
+		try {
+			Arrays.sort(_prefs);
+			InputStream input = getClass().getResourceAsStream("/kima_names.txt");
+			BufferedReader in = new BufferedReader(new InputStreamReader(input));
+			String str;
 
-    public ExampleKimaNERPlugin() {
-    try {
-        Arrays.sort(_prefs);
-        InputStream input = getClass().getResourceAsStream("/kima.txt");
-        BufferedReader in = new BufferedReader(new InputStreamReader(input));
-        String str;
+			List<String> list = new ArrayList<String>();
+			while((str = in.readLine()) != null){
+				list.add(str);
+			}
+			_aliases = new HashSet<String>(list);
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+			_aliases = new HashSet();
+		}
+	}
 
-        List<String> list = new ArrayList<String>();
-        while((str = in.readLine()) != null){
-            list.add(str);
-        }
-        _aliases = new HashSet<String>(list);
-    }
-    catch (IOException e) {
-        e.printStackTrace();
-        _aliases = new HashSet();
-    }
-    }
+	@Override
+	public String getName() {
+		return "Example Kima NER Plugin";
+	}
 
-    @Override
-    public String getName() {
-    return "Example Kima NER Plugin";
-    }
+	@Override
+	public String getDescription() {
+		return "An attempt to use Kima with the Recogito NER plugin interface.";
+	}
 
-    @Override
-    public String getDescription() {
-    return "An attempt to use Kima with the Recogito NER plugin interface.";
-    }
+	@Override
+	public String getOrganization() {
+		return "Pelagios Commons";
+	}
 
-    @Override
-    public String getOrganization() {
-    return "Pelagios Commons";
-    }
+	@Override
+	public String getVersion() {
+		return "1.0";
+	}
 
-    @Override
-    public String getVersion() {
-    return "1.0";
-    }
+	@Override
+	public List<String> getSupportedLanguages() {
+		List<String> list = new ArrayList<String>();
+		list.add("he");
 
-    @Override
-    public List<String> getSupportedLanguages() {
-    List<String> list = new ArrayList<String>();
-    list.add("he");
+		return list;
+	}
 
-    return list;
-    }
+	/**
+	 * For testing and demonstration purposes, the dummy NER plugin
+	 * reports a 'LOCATION' entity for every uppercase word. Don't consider
+	 * this a sensible
+	 */
+	@Override
+	public List<Entity> parse(String text) {
+		String[] words = text.split("\\s+");
+		List<Entity> phrases = new ArrayList<Entity>();
 
-    /**
-    * For testing and demonstration purposes, the dummy NER plugin
-    * reports a 'LOCATION' entity for every uppercase word. Don't consider
-    * this a sensible
-    */
-    @Override
-    public List<Entity> parse(String text) {
-    String[] words = text.split("\\s+");
-    List<Entity> phrases = new ArrayList<Entity>();
+		if (words.length == 0)
+			return phrases;
 
-    if (words.length == 0)
-    return phrases;
+		int runningIdx = 0;
+		int runningOffset = 0;
 
-    int runningIdx = 0;
-    int runningOffset = 0;
+		while (runningIdx < words.length) {
+			String nextWord = words[runningIdx];
+			runningOffset = text.indexOf(nextWord, runningOffset);
 
-    while (runningIdx < words.length) {
-      String nextWord = words[runningIdx];
-      runningOffset = text.indexOf(nextWord, runningOffset);
+			if (hasWord(nextWord, _aliases)) {
+				phrases.add(new Entity(
+							nextWord,
+							EntityType.LOCATION,
+							runningOffset
+							));
+			}
+			else {
+				for(int j = 0; j < MAX_WORDS && runningIdx + j < words.length; ++j) {
+					List<String> subWords = Arrays.asList(words).subList(runningIdx, runningIdx + j);
+					String expr = String.join(" ", subWords);
+					if (hasWord(expr, _aliases)) {
+						phrases.add(new Entity(
+									expr,
+									EntityType.LOCATION,
+									runningOffset
+									));
+					}
+				}
+			}
+			runningIdx++;
+			runningOffset += nextWord.length();
+		}
 
-      if (hasWord(nextWord, _aliases)) {
-        phrases.add(new Entity(
-        nextWord,
-        EntityType.LOCATION,
-        runningOffset
-        ));
-      }
-
-      runningIdx++;
-      runningOffset += nextWord.length();
-    }
-
-    return phrases;
-    }
+		return phrases;
+	}
 
 }
